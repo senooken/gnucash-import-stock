ea0c031ad8aa568de26f77fb08ce742a5d27f971
SENOO, Ken <contact@senooken.jp>
2020-08-12 07:57:23 +0900

    改行コードをcp932に統一

--- gnucash-import-stock.py
+++ gnucash-import-stock.py
@@ -77,7 +77,7 @@ for file in pathlib.Path('.').glob('*.csv'):
 with open(trade_csv, newline='', encoding='cp932') as trade_file, \
      open(pay_csv, newline='', encoding='cp932') as pay_file, \
      open('一覧.csv', 'w', newline='', encoding='cp932') as list_file, \
-     open('import.csv', 'w', newline='') as import_file:
+     open('import.csv', 'w', newline='', encoding='cp932') as import_file:
     ## 株式約定履歴.csvの取り込み
     SKIP_HEADER_LINES = 7
     for i in range(SKIP_HEADER_LINES):
